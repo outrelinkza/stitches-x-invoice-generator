e0c0b8ca11837338017b5b7e58253e6f866e0a69
outrelinkza <outrelink@gmail.com>
2025-09-13 11:48:06 +0200

    Remove test file and ensure clean deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1462,13 +1462,13 @@ export default function Home() {
                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                           </svg>
                           Basic templates
-                        </li>
+          </li>
                         <li className="flex items-center text-white/80">
                           <svg className="w-5 h-5 text-green-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                           </svg>
                           PDF download
-                        </li>
+          </li>
                         <li className="flex items-center text-white/80">
                           <svg className="w-5 h-5 text-green-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -1682,7 +1682,7 @@ export default function Home() {
                   >
                     Subscribe
                   </button>
-                </div>
+        </div>
                 <p className="text-xs opacity-70">No spam, just curated content and exclusive access.</p>
               </div>
             </div>
